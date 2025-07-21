0e9a53fb1a016c2ac6281706a7ed55c646a2b838
SalmanSakri <96472384+SalmanSakri@users.noreply.github.com>
2025-07-21 11:15:22 +0530

    deploy

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,14 +1,17 @@
-import  express,{Router} from 'express';
+import express, { Router } from 'express';
 import serverless from "serverless-http"
 
 const app = express();
-const router =Router()
+const router = Router()
+
+app.use(express.json())
+
 
 // Define a basic route
 router.get('/', (req, res) => {
-    res.send('Hello from Express!');
+    res.sendFile(__dirname + 'index.html');
 });
 
 
-app.use("/api/",router);
-export const handler=serverless(app)
+app.use("/api/", router);
+export const handler = serverless(app)
