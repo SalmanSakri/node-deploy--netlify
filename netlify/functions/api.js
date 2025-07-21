1cbde61572ccf764d6e950d0c53661f6cb2694a8
SalmanSakri <96472384+SalmanSakri@users.noreply.github.com>
2025-07-21 10:57:29 +0530

    deploy in githup

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,17 +3,12 @@ import serverless from "serverless-http"
 
 const app = express();
 const router =Router()
-const port = 3000; // You can choose any available port
 
 // Define a basic route
 router.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
-// Start the server
-router.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
 
 app.use("/api/",router);
 export const handler=serverless(app)
